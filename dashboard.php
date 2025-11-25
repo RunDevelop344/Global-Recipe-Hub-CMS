376f82d3c1295625e5f99151706a99bd1adff266
Ssimran-5 <ssimran5@rrc.ca>
2025-11-24 20:42:42 -0600

    dasboard created

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php
+require 'admin.php';
+require 'connect.php';
+?>
+
+<h1>Admin Dashboard</h1>
+
+<ul>
+    <li><a href="create.php">Create Recipe</a></li>
+    <li><a href="manage_recipes.php">Manage All Recipes</a></li>
+    <li><a href="manage_comments.php">Moderate Comments</a></li>
+    <li><a href="users.php">View Users</a></li>
+</ul>
