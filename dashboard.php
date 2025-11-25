a9c37e191de23d8f5ab829f80698e6dbd389d4a6
Ssimran-5 <ssimran5@rrc.ca>
2025-11-24 21:33:52 -0600

    dashboard updaetd

--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,15 @@
 <?php
 require 'admin.php';
+require 'header.php';
 require 'connect.php';
 ?>
 
 <h1>Admin Dashboard</h1>
 
+<h2>Recipe Management</h2>
 <ul>
-    <li><a href="create.php">Create Recipe</a></li>
-    <li><a href="manage_recipes.php">Manage All Recipes</a></li>
-    <li><a href="manage_comments.php">Moderate Comments</a></li>
-    <li><a href="users.php">View Users</a></li>
+    <li><a href="create.php">➕ Add New Recipe</a></li>
+    <li><a href="dashboard_recipes.php">📚 Edit Existing Recipes</a></li>
 </ul>
+
+<?php include 'footer.php'; ?>
